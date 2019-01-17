1a44c4515e5a5595f025da8dc3affd75968e3246
raj-naman <naman11396@gmail.com>
2019-01-17 10:04:17 +0530

    Add Program

---
+++ Add.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class JavaExample {
+
+    public static void main(String[] args) {
+
+    	int nm1  = 5 , nm2 = 8;
+        System.out.println(nm1 + nm2);
+    }
+}
+
+
